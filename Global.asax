b802342d1f9ab1891b77fbcdd8ebe01f3b210a0b
Nicolas Rolando Saez Matamala <nrsaez@sacyr.com>
2022-05-12 14:37:22 -0400

    commit inicial

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SIC_AVO_DEV.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SIC_AVO_DEV.Startup))]
+namespace SIC_AVO_DEV
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
