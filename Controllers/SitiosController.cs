8f8b9e825289385eb34312e7b8d04d0904842def
Nicolas Rolando Saez Matamala <nrsaez@sacyr.com>
2022-05-18 14:56:49 -0400

    List sitios

--- Controllers/SitiosController.cs
+++ Controllers/SitiosController.cs
@@ -2,33 +2,56 @@
 using Microsoft.AspNet.Identity.EntityFramework;
 using SIC_AVO_DEV.Models;
 using System;
+using Data;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SIC_AVO_DEV.Model;
 
 namespace SIC_AVO_DEV.Controllers
 {
     public class SitiosController : Controller
     {
+        ApplicationDbContext context;
         // GET: Sitios
         public JsonResult GetListSitios()
         {
+            Data.SitiosController sitiosController = new Data.SitiosController();
+            TipoSitiosController tipoSitiosController = new TipoSitiosController();
+            var listaSitios = sitiosController.GetAllSitosActive();
+            var listaTipoSitio = tipoSitiosController.GetAllTipoSitosActive();
+            List<SitioModel> sitioModel = new List<SitioModel>();
 
-            ApplicationDbContext context = new ApplicationDbContext();
-            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            sitioModel = (from si in listaSitios
+                          join ti in listaTipoSitio on si.Id equals ti.Id
+                          select new SitioModel
+                          {
+                              Id = si.Id,
+                              Nombre = si.Nombre,
+                              Descripcion = si.Descripcion,
+                              URL = si.URL,
+                              NombreTipoSitio = ti.Nombre,
+                              IdModulo = si.IdModulo,
+                              IdSubMenuPadre = si.IdSubMenuPadre,
+                              IdTipoSitio = ti.Id,
+                              
+                          }).ToList();
 
-            //aquí obtienes la lista de datos de los sitios y la entegas a un json
+            IEnumerable<SitioModel> sitioModels = sitioModel;
 
-            var json = new
+            return Json(new { data = sitioModels });
+        }
+
+        public ActionResult Simple()
+        {
+            List<Sitio> all = new List<Sitio>();
+            using (SIC_NSEntities dc = new SIC_NSEntities())
             {
-                error = true,
-                message = "Ocurrió un problema al generar el Excel"
-            };
-
-            var jsonResult = Json(json, "text/html", System.Text.Encoding.UTF8,JsonRequestBehavior.AllowGet);
-            jsonResult.MaxJsonLength = int.MaxValue;
-            return jsonResult;
+                all = dc.Sitio.OrderBy(a => a.Id).ToList();
+                //all = dc.tipo.OrderBy(a => a.Id).ToList();
+            }
+            return View(all);
         }
 
 
---
+++ Model/SitioModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SIC_AVO_DEV.Model
+{
+    public class SitioModel
+    {
+        public long Id { get; set; }
+        public string Nombre { get; set; }
+        public string URL { get; set; }
+        public long IdTipoSitio { get; set; }
+        public string Descripcion { get; set; }
+        public bool Activo { get; set; }
+        public System.DateTime FechaCreacion { get; set; }
+        public long IdUsuarioCreador { get; set; }
+        public long IdUsuarioCreadorHistorial { get; set; }
+        public Nullable<long> IdSubMenuPadre { get; set; }
+        public Nullable<long> IdModulo { get; set; }
+
+
+        public string NombreTipoSitio { get; set; }
+    }
+}
