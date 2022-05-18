20ecd0d6790861bf049a634c26a882b35668c8ee
Nicolas Rolando Saez Matamala <nrsaez@sacyr.com>
2022-05-18 10:22:04 -0400

    Capa data

---
+++ Controllers/SitiosController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using SIC_AVO_DEV.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SIC_AVO_DEV.Controllers
+{
+    public class SitiosController : Controller
+    {
+        // GET: Sitios
+        public JsonResult GetListSitios()
+        {
+
+            ApplicationDbContext context = new ApplicationDbContext();
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            //aquí obtienes la lista de datos de los sitios y la entegas a un json
+
+            var json = new
+            {
+                error = true,
+                message = "Ocurrió un problema al generar el Excel"
+            };
+
+            var jsonResult = Json(json, "text/html", System.Text.Encoding.UTF8,JsonRequestBehavior.AllowGet);
+            jsonResult.MaxJsonLength = int.MaxValue;
+            return jsonResult;
+        }
+
+
+    }
+}
---
+++ ViewModels/RolesViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+
+
+namespace SIC_AVO_DEV.Models
+{
+    public class RolesViewModel
+    {
+        [Display(Name = "id")]
+        public string Id { get; set; }
+
+        [Display(Name = "Rol")]
+        public string Name { get; set; }
+    }
+}
